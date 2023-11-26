ac2009b2e8276522fc030a2e9ec697f5d51725e8
NKvap <126457014+NKvap@users.noreply.github.com>
2023-11-26 14:38:10 +0500

    Add files via upload

---
+++ MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiApp3
+{
+    internal static class Visibles
+    {
+        public static void UpdateSegments(this Indicators indicators, BoxView topSegment, BoxView leftTopSegment,
+            BoxView rightTopSegment, BoxView middleSegment, BoxView leftBottomSegment, BoxView rightBottomSegment, BoxView bottomSegment)
+        {
+            topSegment.IsVisible = indicators.TopSegment;
+            leftTopSegment.IsVisible = indicators.LeftTopSegment;
+            rightTopSegment.IsVisible = indicators.RightTopSegment;
+            middleSegment.IsVisible = indicators.MiddleSegment;
+            leftBottomSegment.IsVisible = indicators.LeftBottomSegment;
+            rightBottomSegment.IsVisible = indicators.RightBottomSegment;
+            bottomSegment.IsVisible = indicators.BottomSegment;
+        }
+
+    }
+    
+}
